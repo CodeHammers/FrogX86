39f1dfeef6a029853f670848bdd08421a8646bbb
Kareem-Emad <kareememad400@gmail.com>
2017-11-25 17:35:29 +0200

    fixed shift in road

--- Project.asm
+++ Project.asm
@@ -12,6 +12,7 @@ include Shift.inc
 tiles db 640 dup(3)    ;Code byte for the content of each tile 0:Status | 1:Frog | 2:Log main | 3:Water | 4:Pavement | 5:Road | 6: log beg. | 7: log end | 8: Car rear | 9: Car front | 10: endPoint
 
 frogPos dw 610
+fakePos dw 610
 BoundsFlag db ?
 
 ;oldCode db 4
@@ -80,9 +81,9 @@ GameLoop:
     Shift bx 224 0 frogPos
 	Shift bx 256 1 frogPos
 	Shift bx 288 0 frogPos
-    Shift bx 352 1 frogPos
-    Shift bx 384 0 frogPos
-    Shift bx 416 1 frogPos
+    Shift bx 352 1 fakePos
+    Shift bx 384 0 fakePos
+    Shift bx 416 1 fakePos
 	;Shift bx 96 1 frogPos	
 	;Shift bx 112 0 frogPos	
 	;Shift bx 128 1 frogPos
