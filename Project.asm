ea869bbbb61d94f9616e7de24c7df288d943b01d
SayedAlesawy <Sayed.Kotb97@eng-st.cu.edu.eg>
2017-12-19 18:49:33 +0200

    Adding comments

--- Project.asm
+++ Project.asm
@@ -22,7 +22,7 @@ chatFlag db ?
 readyflag db 0
 readyflag2 db 0
 levelflag db ?
-;---------------------------------------------------------------
+;------------------Chat module variables---------------------
         ToBeSent db ?,'$'
         ToBeReceived db '$','$'
         Divider db '--------------------------------------------------------------------------------$'
@@ -37,7 +37,8 @@ levelflag db ?
         ApplyNewLine db 0Dh  
         NUllChar db ' $'
         GoodByeMessage db 'Goodbye, we wish that you have enjoyed your time!$'
-;---------------------------------------------------------------
+
+;------------------In game chat variables-------------------------
         ToBeSentGame db ?,'$'
         ToBeReceivedGame db '$','$'
         row1Game db 1
@@ -51,7 +52,6 @@ levelflag db ?
         scrollRow dw ?
 ;---------------------------------------------------------------
 
-;---------------------------------------------------------------
 
 ;------MainMenu\IntroScreen\StatusBar----------------
 mes1 db '*To start Frogx86 press ENTER','$'
@@ -94,11 +94,15 @@ delayLoops db 4
 mov ax,@data
 mov ds,ax
 
+;Initializing the serial port with the proper configuration
 portinitialization
+
+;Turn the console into video mode
 mov ah,0
 mov al,13h
 int 10h
 
+;The intro screen that provides options ot the user
 IntroScreen mes3,mes4,mes5,MyBuffer1,PlayerName1,ActualSize1
         MOV AX,0600H    ;06 TO SCROLL & 00 FOR FULLJ SCREEN
         MOV BH,00H     
@@ -110,9 +114,10 @@ IntroScreen mes3,mes4,mes5,MyBuffer1,PlayerName1,ActualSize1
 	mov cl,6
 	mov ch,0
 
+;Exchange the names with the other player, give it my name and take his.
 NameLoop:
-	send [si] 
-	receive3ady fakevalue
+	send [si]             ;Send a byte of my name
+	receive3ady fakevalue ;Receive a byte of his name
 	mov al,fakevalue
 	mov [di],al
 	inc si
@@ -122,9 +127,11 @@ NameLoop:
 
 MainMenu:
 
+;Turn into video mode for the game to start
 mov ah,0
 mov al,13h
 int 10h
+
 ;MainMenu\IntroScreen------------
 MainMenu mes1,mes2,mes8,mes9,chatFlag
         MOV AX,0600H    ;06 TO SCROLL & 00 FOR FULLJ SCREEN
@@ -136,10 +143,7 @@ MainMenu mes1,mes2,mes8,mes9,chatFlag
 
 	cmp chatFlag,1
 	je BeginChat 
-	
-	;CALL CHAT MACRO
-    ;jmp BeginChat
-	
+		
 LevelMenu mes10,mes11,levelflag
 ;-----------------------------------
 		MOV AX,0600H    ;06 TO SCROLL & 00 FOR FULLJ SCREEN
@@ -158,6 +162,7 @@ gamebegin:
     mov cx,0
     mov xpos,0
     mov ypos,0
+
     drawBackGround:       
         mov bx, offset tiles
         add bx,cx
@@ -185,19 +190,16 @@ GameLoop:  ;This loop gets Called every loop till player wins
     mov xpos,0
     mov ypos,0
    
-	;cmp delayLoops,0
-	;jnz DelayedLoop
-	;Shifting the rows
 	 mov bh,0
 	 mov bl,tiles
     
-	;Shift bx 96 0 frogPos,frogPos2    
+	    
     Shift bx 128 1 frogPos,frogPos2     ;---------This block shifts the rows of water and cars
 	Shift bx 160 0 frogPos,frogPos2
 	Shift bx 192 1 frogPos,frogPos2
     Shift bx 224 0 frogPos,frogPos2
 	Shift bx 256 1 frogPos,frogPos2
-	;Shift bx 288 0 frogPos,frogPos2
+	
 	Shift bx 320 0 fakePos,fakePos
     Shift bx 352 1 fakePos,fakePos
     Shift bx 384 0 fakePos,fakePos
@@ -208,31 +210,26 @@ GameLoop:  ;This loop gets Called every loop till player wins
     Shift bx 544 1 fakePos,fakePos
     Shift bx 576 0 fakePos,fakePos
 
-	
-	; mov delayLoops,4    ;------This block makes sure that shifting doens't happen every frame so the game is easier
-	; DelayedLoop:
-	; dec delayLoops
-    ;mov direction,0
-	
-	receive3ady direction
+	receive3ady direction  ;receive the movement direction from the other player
 	TakeGameInput frogPos,BoundsFlag  ;Take the input from users
-    cmp BoundsFlag,5
+    cmp BoundsFlag,5   ;If the bounds flag has a value greater than 5, then it's a character.
     jbe normal
 
-    mov al,BoundsFlag
+    mov al,BoundsFlag     ;move the character into the to be sent variable for sending and printing
     mov ToBeSentGame, al 
-    MOVECURSOR row1Game,col1Game
-    ShowMessage ToBeSentGame
-    inc col1Game 
-    cmp col1Game,40
+    MOVECURSOR row1Game,col1Game ;Move the cursor into the next position
+    ShowMessage ToBeSentGame ;display the character
+    inc col1Game     ;Increment the current column.
+    cmp col1Game,40  ;Check if we reached the end of the current line
     jne normal 
-    mov col1Game,0
-    mov scrollRow,8
+    mov col1Game,0   ;If yes, set the cursor back to the beginning of the line
+    mov scrollRow,8  ;And scroll up by 1 line
     scrollVideo scrollRow
 
     normal:
-    send BoundsFlag 
+    send BoundsFlag  ;Send whatever in the boundflag to the other user. 
 
+    ;Check the movements of the other player and apply them to the copy frog
     cmp direction,1
     je up 
     cmp direction,2
@@ -268,19 +265,19 @@ GameLoop:  ;This loop gets Called every loop till player wins
         jmp beed 
 
     character:
-        mov al, direction
+        mov al, direction   ;Move the receieved character to be printed
         mov ToBeReceivedGame, al 
-        MOVECURSOR row2Game,col2Game
-        ShowMessage ToBeReceivedGame
-        inc col2Game
-        cmp col2Game,40
+        MOVECURSOR row2Game,col2Game  ;Move the cursor into the next position
+        ShowMessage ToBeReceivedGame  ;display the character
+        inc col2Game  ;Increment the current column.
+        cmp col2Game,40 ;Check if we reached the end of the current line
         jne beed 
-        mov col2Game,0
+        mov col2Game,0  ;If yes, set the cursor back to the beginning of the line
         mov scrollRow,16
-        scrollVideo scrollRow
+        scrollVideo scrollRow ;And scroll up by 1 line
 
     beed:    
-        mov BoundsFlag,0
+        mov BoundsFlag,0 ;reset the boundsflag
 
     lea bx,tiles        ;Check foreach Frog new Position and check if dead
 	add bx , frogPos
@@ -301,32 +298,34 @@ GameLoop:  ;This loop gets Called every loop till player wins
 	lea bx,tiles
 	add bx,frogPos
 	mov al,[bx]
-	cmp al,10
+	cmp al,10    ;Check of the current spot is a winning spot
 	jne Alive2
-	mov [bx],3
-	mov frogPos,620
-	inc playerScore1
+	mov [bx],3   ;If yes, turn it into water (make it disappear, not usuable by other players)
+	mov frogPos,620  ;Reset the frog position
+	inc playerScore1 ;Increment the player's score
 	inc PlayerScore1Num
-	mov [bx+1],3
-	mov [bx-1],3
+	mov [bx+1],3  ;If x is yellow, check if x+1 is yellow (Used to hide both yellow spots in level 1)
+	mov [bx-1],3  ;If x is yellow, check if x-1 is yellow (Used to hide both yellow spots in level 1)
+
 	Alive2: 
 	lea bx,tiles
 	add bx,frogPos2
 	mov al,[bx]
-	cmp al,10
+	cmp al,10   ;Check of the current spot is a winning spot
 	jne CheckWon
-	mov [bx],3
-	mov frogPos2,620
-	inc playerScore2
+	mov [bx],3  ;If yes, turn it into water (make it disappear, not usuable by other players)
+	mov frogPos2,620  ;Reset the frog position
+	inc playerScore2  ;Increment the player's score
 	inc PlayerScore2Num
-	mov [bx+1],3
-	mov [bx-1],3
-	CheckWon:               ;--Check the winner
+	mov [bx+1],3 ;If x is yellow, check if x+1 is yellow (Used to hide both yellow spots in level 1)
+	mov [bx-1],3 ;If x is yellow, check if x-1 is yellow (Used to hide both yellow spots in level 1)
+
+	CheckWon:               ;--Check the winner (And print his name)
 	mov al,PlayerScore1Num
 	mov ah,PlayerScore2Num
 	
 	cmp al,3d
-	jge Player1Won
+	jge Player1Won  ;if the player score is above 3, then he is a winner
 	cmp ah,3d
 	jl Continue
 	
@@ -465,6 +464,7 @@ GameLoop:  ;This loop gets Called every loop till player wins
 	inc dx
 	inc cx
 	DrawHorizontalLine cx, dx, 6, 06h
+
 	;water filling
 	mov cx,xpos
 	mov dx,ypos
@@ -811,34 +811,36 @@ GameLoop:  ;This loop gets Called every loop till player wins
 jmp GameLoop 
 
 BeginChat:
+        ;Switch to text mode
         mov ah,00h
 		mov al,02h
 		int 10h
-        portinitialization  
-        CLEAR_SCREEN_UP
-        MOVECURSOR 0,0
+
+        portinitialization  ;Initialize the port
+        CLEAR_SCREEN_UP     ;Clear the screen
+        MOVECURSOR 0,0      ;Reset the cursor
         
-        MOVECURSOR 12,0
+        MOVECURSOR 12,0     ;Move the cursor to the middle of the screen and divide it into two halves
         ShowMessage Divider
 
-        MOVECURSOR 0,0
+        MOVECURSOR 0,0      ;Print and first player name
         ShowMessage PlayerName1
         
-        MOVECURSOR 13,0
+        MOVECURSOR 13,0     ;Print the second player name
         ShowMessage PlayerName2
         
-        MOVECURSOR 1,0
+        MOVECURSOR 1,0   ;Reset the cursor back at the beginning of the chat window
         
         chat:
-            mov ToBeReceived,'$'
+            mov ToBeReceived,'$'  ;reset to default
 
-            mov ah,1
+            mov ah,1   ;Read input into buffer
             int 16h
-            jnz Se 
-            jmp Re   
+            jnz Se     ;If a key was pressed, let's check it and send it
+            jmp Re     ;Otherwise let's see if we have something to receive.
             
             Se: 
-                mov ah,00   
+                mov ah,00  ;Consume the buffer  
                 int 16h
 
                 cmp ah,1ch
@@ -852,65 +854,65 @@ BeginChat:
                  
                 mov ToBeSent,al
                 
-                MOVECURSOR row1,col1
-                ShowMessage ToBeSent 
+                MOVECURSOR row1,col1 ;Move the cursor to the next position
+                ShowMessage ToBeSent ;Print the character I have just typed
             
-                send ToBeSent
+                send ToBeSent  ;Send it to the other player
                 
-                inc col1 
-                cmp col1,80
-                je ResetCol1
+                inc col1    ;Increment the current column
+                cmp col1,80 ;Check if we have reached the end of the lines
+                je ResetCol1 ;Reset values
                 BackCol1:
                 jmp Re 
              
              BackSpace1: 
-                cmp col1,0
+                cmp col1,0  ;If the cursor is at the beginning of the line and the use press backspace, ignore it
                 je Re  
-                send ah 
-                dec col1 
-                MOVECURSOR row1,col1
+                send ah  ;Otherwise tell the other PC to backspace as well
+                dec col1 ;decrement the col to the previous position (to fill the gap)
+                MOVECURSOR row1,col1 ;Move the cursor and print a null character
                 ShowMessage NUllChar
-                MOVECURSOR row1,col1
+                MOVECURSOR row1,col1 ;Show cursor
                 jmp Re
 
              enter:
-                mov col1,0
-                inc row1
-                cmp row1,12
-                je ResetRow1Enter
+                mov col1,0 ;reset the col
+                inc row1   ;increment row
+                cmp row1,12 ;check if I have consumed my half
+                je ResetRow1Enter ;if yes, reset and scroll
                 BackRow1Enter:
                 MOVECURSOR row1,col1
-                send ApplyNewLine 
+                send ApplyNewLine ;Send the enter to the other user
                 
              Re:
-                receiveChat ToBeReceived
+                receiveChat ToBeReceived  ;receive what's on the serial
                  
-                cmp ToBeReceived,'$'
+                cmp ToBeReceived,'$'  ;if it's still a dollar a sign, then I have received nothing, ignore it
                 je chat 
 
-                cmp ToBeReceived, 0Eh
+                cmp ToBeReceived, 0Eh ;If I have received a backspace, reflect the changes
                 je Backspace2
 
-                cmp ToBeReceived, 0Dh 
+                cmp ToBeReceived, 0Dh ; If Have received an enter, reflect the changes
                 je Enter2
 
-                MOVECURSOR row2,col2
-                ShowMessage ToBeReceived
-                inc col2
-                cmp col2,80
-                je ResetCol2
+                MOVECURSOR row2,col2  ;If i have received a character, move the cursor to the next position
+                ShowMessage ToBeReceived ;And print it
+                inc col2  ;Increment the current col
+                cmp col2,80 ;Check end of line
+                je ResetCol2 ;If end of line, reset
                 BackCol2: 
         
           jmp chat
           
-          Backspace2:
+          Backspace2: ;same as backspace1
             dec col2
             MOVECURSOR row2,col2
             ShowMessage NUllChar
             MOVECURSOR row2,col2
             jmp chat  
 
-          Enter2:
+          Enter2:  ;same as enter1
             mov col2,0
             inc row2 
             cmp row2,24
@@ -920,13 +922,13 @@ BeginChat:
             jmp chat 
 
           ResetEnter2:
-            scroll from2,to2 
-            MOVECURSOR 12,0
-            ShowMessage Divider
-            mov row2,23
+            scroll from2,to2 ;If an enter caused me to consume all my space, then scroll one line up
+            MOVECURSOR 12,0  ;Reset the cursor
+            ShowMessage Divider ;Redraw the divider, propaply useless
+            mov row2,23 ;reset the row
             jmp BackEnter2
 
-          ResetCol1:
+          ResetCol1: ;same idea as other resets
             mov col1,0
             inc row1
             cmp row1,12
@@ -934,7 +936,7 @@ BeginChat:
             BackRow1:
             jmp BackCol1
           
-          ResetCol2:
+          ResetCol2: ;same idea as other resets
             mov col2,0
             inc row2
             cmp row2,24
@@ -942,27 +944,27 @@ BeginChat:
             BackRow2:
             jmp BackCol2
           
-          ResetRow1:
+          ResetRow1: ;same idea as other resets
             scroll from1,to1
             mov row1,11
             jmp BackRow1
           
-          ResetRow2:
+          ResetRow2: ;same idea as other resets
             scroll from2,to2 
             MOVECURSOR 12,0
             ShowMessage Divider
             mov row2,23
             jmp BackRow2
             
-          ResetRow1Enter:
+          ResetRow1Enter: ;same idea as other resets
             scroll from1,to1
             mov row1,11
             jmp BackRow1Enter
             
-          escape:
+          escape: ;If the user quits chat, move to the main menu
             CLEAR_SCREEN_UP
             MOVECURSOR 0,0
             ShowMessage GoodByeMessage
-			jmp MainMenu:
+			jmp MainMenu
 
 hlt               
